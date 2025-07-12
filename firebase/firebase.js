d3c39ac29b7851fb787d0519843d6d892b76d119
lavanyatomarr <lavanyatomarr@gmail.com>
2025-07-12 10:28:24 +0530

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,1 +1,29 @@
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyBgbizuirtNwZvOuflvLd66tw-lzLZ1Hmo",
+  authDomain: "polaroid-ar-memory.firebaseapp.com",
+  projectId: "polaroid-ar-memory",
+  storageBucket: "polaroid-ar-memory.firebasestorage.app",
+  messagingSenderId: "285625088296",
+  appId: "1:285625088296:web:0fc4f69380879df2c2b81d",
+  measurementId: "G-5STLG9BYYJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+  authDomain: "polaroid-ar-memory.firebaseapp.com",
+  projectId: "polaroid-ar-memory",
+  storageBucket: "polaroid-ar-memory.firebasestorage.app",
+  messagingSenderId: "285625088296",
+  appId: "1:285625088296:web:0fc4f69380879df2c2b81d",
+  measurementId: "G-5STLG9BYYJ"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+  
